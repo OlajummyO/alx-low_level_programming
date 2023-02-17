288e26bdc9294aa571a7bb5ed7f33e5a13329fee
OlajummyO <Olajummy.olajumoke@gmail.com>
2023-02-17 12:58:47 -0800

    new changes to 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
+#include<unistd.h>
 /** 
- * main - prints " that piece of art is useful" - Dora Korpar, 2015-10-19 "
- * followed by a new line to the standard error
+ * main - Entry point
+ *
  * Return: Always 1 (Sucess)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
 	return (1);
 }
