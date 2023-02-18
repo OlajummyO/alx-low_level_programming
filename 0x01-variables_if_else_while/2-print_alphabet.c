f490c559658e3552fcd10129209ad9070ab9839e
OlajummyO <Olajummy.olajumoke@gmail.com>
2023-02-18 14:00:09 -0800

    program that run alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main - Prints the alhabets in lowercase
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char letter:
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
