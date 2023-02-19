b80d7f17c01e51a95668e506950ea56dfaa63fc7
OlajummyO <Olajummy.olajumoke@gmail.com>
2023-02-19 09:13:50 -0800

    script that print alhabet in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints alphabet in lowwercase, and then in uppercase.
+ * main - Prints the alphabet in lowercase, and then in uppercase.
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
 	char letter;
@@ -12,7 +12,7 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	for(letter = 'A'; letter <= 'Z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
